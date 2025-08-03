13f233bc3f5d072ddab203efb57ee35b7355434f
Volker Goehler <volker.goehler@informatik.tu-freiberg.de>
2025-08-03 21:48:38 +0200

    main modified

--- src/liascript_img_makro_gen/main.py
+++ src/liascript_img_makro_gen/main.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+minimal runner for keyword_extraction module
+"""
+
+from liascript_img_makro_gen.create_makros import generate 
 
 def app():
-    print("Hello, World!")
+    generate()
+
+if __name__ == "__main__":
+    main()
