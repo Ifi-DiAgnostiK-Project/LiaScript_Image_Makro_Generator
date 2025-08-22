709705c2b47356be0dad0d95baaa13b2ebdf6df3
Volker Goehler <volker.goehler@informatik.tu-freiberg.de>
2025-08-22 23:16:15 +0200

    refactored main script to include argument parsing and config handling

--- src/liascript_img_makro_gen/main.py
+++ src/liascript_img_makro_gen/main.py
@@ -4,10 +4,28 @@
 minimal runner for keyword_extraction module
 """
 
-from liascript_img_makro_gen.create_makros import generate 
+import argparse
+from liascript_img_makro_gen.generate_makros import LiaScriptMakroGenerator
+from liascript_img_makro_gen.confighandler import ConfigLoader
 
-def app():
-    generate()
+def main():
+    parser = argparse.ArgumentParser(
+        description="Run the keyword extraction script with a custom config file location."
+    )
+    parser.add_argument(
+        "--config",
+        required=True,
+        help="Path to the configuration file.",
+        default="config.yaml"
+    )
+    
+    # Parse the command line arguments
+    args = parser.parse_args()
+    
+    # Load the configuration and generate the makros using the provided config file
+    loader = ConfigLoader(args.config)
+    config = loader.load_config()
+    LiaScriptMakroGenerator(config)
 
 if __name__ == "__main__":
     main()
