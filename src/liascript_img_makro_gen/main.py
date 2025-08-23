60635e1350a12b13a34615dba2755e87aef19a26
Volker Goehler <volker.goehler@informatik.tu-freiberg.de>
2025-08-23 17:34:20 +0200

    refactored main script to initialize generator instance and invoke `generate_makros` method

--- src/liascript_img_makro_gen/main.py
+++ src/liascript_img_makro_gen/main.py
@@ -5,6 +5,7 @@ minimal runner for keyword_extraction module
 """
 
 import argparse
+
 from liascript_img_makro_gen.generate_makros import LiaScriptMakroGenerator
 from liascript_img_makro_gen.confighandler import ConfigLoader
 
@@ -25,7 +26,8 @@ def main():
     # Load the configuration and generate the makros using the provided config file
     loader = ConfigLoader(args.config)
     config = loader.load_config()
-    LiaScriptMakroGenerator(config)
+    generator = LiaScriptMakroGenerator(config)
+    generator.generate_makros()
 
 if __name__ == "__main__":
     main()
